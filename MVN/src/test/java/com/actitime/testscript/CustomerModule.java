bd2fc0d37908bc9db2008175f3ed7b29d5abb9b0
Sai Kiran <Sai Kiran@naveen>
2024-10-23 12:38:30 +0530

    FIRST COMMIT

---
+++ MVN/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,16 @@
+package com.actitime.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void createCustomer() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
